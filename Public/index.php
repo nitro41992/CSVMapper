b4aedd3069019e52b780c420e477aa51cfb31566
kuchiman <kuchiman@njit.edu>
2019-02-10 00:31:51 -0500

    Feature: Read .csv file into one row array (In Progress)

--- Public/index.php
+++ Public/index.php
@@ -6,4 +6,56 @@
  * Time: 9:23 PM
  */
 
-echo 'Test Github integration.';
+
+main::start();
+
+class main {
+
+    static public function start() {
+            $file = '/home/kuchiman/Documents/Test.csv';
+            //print_r ($file);
+            $records[] = csv::getRecords($file);
+            print_r($records);
+            //$table = html::generateTable($records);
+            //system::printPage($table);
+    }
+
+}
+
+class csv {
+    static public function getRecords($fileName) {
+        if (file_exists($fileName)){
+            //echo ('exists');
+
+            $readFile = file_get_contents($fileName);
+
+            
+            $record[] = explode(',', $readFile);
+
+            return($record);
+
+        } else {
+
+            echo('not exist');
+
+        }
+    }
+
+}
+
+
+class html{
+    static public function generateTable($records) {
+        return $records = 'test';
+    }
+}
+
+
+class system{
+    static public function printPage($page) {
+
+        echo($page);
+
+    }
+
+}
